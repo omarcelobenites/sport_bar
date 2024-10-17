22a55939ad6537728c934f5f82bf100aa4c135a6
omarcelobenites <marcelofelixbenite1985@gmail.com>
2024-10-17 19:25:02 -0300

    feat: alterando api

--- src/serices/api.ts
+++ src/serices/api.ts
@@ -10,7 +10,7 @@ const api = createApi({
       query: () => 'ebac_sports'
     }),
     getFavoritos: builder.query<[], void>({
-      query: () => '/favoritos'
+      query: () => 'favoritos'
     })
   })
 })
